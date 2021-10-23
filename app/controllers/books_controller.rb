62f9d638223f046d7f0a77b42fcc6f00a6df6576
ueyama1995 <shinji160829033205@gmail.com>
2021-10-23 06:06:46 +0000

    マイページまで作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,7 @@ class BooksController < ApplicationController
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to books_path
@@ -19,11 +19,14 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
 
-  def post_image_params
+  def book_params
     params.require(:book).permit(:title, :body)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :post_images, dependent: :destroy
+  has_many :books, dependent: :destroy
 end
