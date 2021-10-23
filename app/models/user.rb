577c702b8f3991dd4d6c4c282fb9b9a8e96972a3
ueyama1995 <shinji160829033205@gmail.com>
2021-10-23 07:03:19 +0000

    [Update] バリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
+  attachment :profile_image
+  validates :name, length: { minimum: 2 }
 end
