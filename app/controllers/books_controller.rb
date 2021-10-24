221e8552bff1b179ed86b04de0bdd98a3f558695
ueyama1995 <shinji160829033205@gmail.com>
2021-10-24 08:16:14 +0000

    [Update] バリデーション完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,9 +7,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      redirect_to books_path
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -19,6 +21,20 @@ class BooksController < ApplicationController
 
   def show
      @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,32 @@ class UsersController < ApplicationController
   def show
      @user = User.find(params[:id])
      @books = @user.books.reverse_order
+     @book = Book.new
   end
 
   def edit
     @user = User.find(params[:id])
   end
 
-  def update
+    def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+     if @user.update(user_params)
+      redirect_to user_path(@user.id), notice: "Profile was successfully updated."
+
+     else
+      render :edit
+     end
+    end
+
+  def create
+    if @user.save
+       flash[:notice]
+      redirect_to user_path(@user.id)
+
+    else
+      @user = User.all
+      render :index
+    end
   end
 
    private
