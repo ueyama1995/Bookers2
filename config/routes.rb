9e06ae24e85dc378be1a5630beca110c7de53c7c
ueyama1995 <shinji160829033205@gmail.com>
2021-11-04 09:13:22 +0000

    応用課題4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   resources :book_comments, only: [:create, :destroy]
    resource :favorites, only: [:create, :destroy]
   end
+   resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :users, only: [:index, :show, :edit, :update]
   get 'home/about' => 'homes#about'
 
