b65bde121390e5d6429f27092e0fe2dae8c299b4
ueyama1995 <shinji160829033205@gmail.com>
2021-11-07 06:08:42 +0000

    応用課題5syuuryou

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,12 +2,14 @@ class SearchesController < ApplicationController
   def search
      @range = params[:range]
      search = params[:search]
-     word = params[:word]
+     @word = params[:word]
     if @range == '1'
-    @user = User.search(search,word)
+    @user = User.search(search,@word)
     #binding.pry
+    elsif @range == '2'
+    @book = Book.search(search,@word)
     else
-    @book = Book.search(search,word)
+      redirect_back(fallback_location: root_path)
     end
   end
 end
