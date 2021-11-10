4a20be7a12a95288c4a3491d2db41762fcf143f3
ueyama1995 <shinji160829033205@gmail.com>
2021-11-10 06:23:01 +0000

    [Finish] 応用課題6 11/10

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,19 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
+    @user = @book.user
+    @book_comment = Book.new
   end
 
   def destroy
-    BookComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @book_comment = @book.book_comments.find(params[:id])
+    @book_comment.destroy!
+    #BookComment.find_by(id: params[:id]).destroy
   end
 
    private
